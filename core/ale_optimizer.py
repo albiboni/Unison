cf879cd2ec9d2a287945d5419dd2e63b3b4b5d41
Alejandro <adanielnoel@gmail.com>
2018-03-17 19:41:20 +0100

    alpha release of ale_optimizer

---
+++ core/ale_optimizer.py
@@ -0,0 +1,21 @@
+"""
+Created by Alejandro Daniel Noel
+"""
+from core.plant_graph.Machine import Machine
+
+
+def optimize_plant(output_machine: Machine):
+    increment = 1.0
+    output_rate = 1.0
+
+    while increment > 0.002:
+        successful = output_machine.set_supplier_rates(output_rate)
+
+        if successful:
+            increment *= 2
+            output_rate += increment
+        else:
+            output_rate -= increment
+            increment *= 0.5
+            output_rate += increment
+    return output_machine
