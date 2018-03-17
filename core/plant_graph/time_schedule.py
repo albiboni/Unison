8c510e79864ab7038b9ed48b3f5500256ea4b1b8
Alejandro <adanielnoel@gmail.com>
2018-03-17 10:56:29 +0100

    Added time schedule calculation

---
+++ core/plant_graph/time_schedule.py
@@ -0,0 +1,20 @@
+"""
+Created by Alejandro Daniel Noel
+"""
+
+
+def create_time_schedule(output_machine):
+    # [process_name, duration, end_time, dependencies, performance_percent]
+    rows = output_machine.get_scheduling(end_time=0.0, output_units_required=1.0)
+
+    # Find starting time
+    min_time = 0.0
+    for row in rows:
+        if row[2] - row[1] < min_time:
+            min_time = row[2] - row[1]
+
+    # Offset all times so that the entire process starts at 0
+    for row in rows:
+        row[2] += abs(min_time)
+
+    return rows
