753d2b2fa91bd217d9724276f2fb53f707170d93
Alejandro <adanielnoel@gmail.com>
2018-03-17 06:00:40 +0100

    Added delays and fixed bugs

--- core/plant_graph/Machine.py
+++ core/plant_graph/Machine.py
@@ -11,7 +11,8 @@ class Machine:
     def __init__(self, name,
                  min_output_rate: float, max_output_rate: float,
                  output_product: Product,
-                 suppliers: List['Machine'] = None, is_on=True, output_rate: float = None,
+                 suppliers: List['Machine'] = None, delays: List[float] = None,
+                 is_on=True, output_rate: float = None,
                  test_suppliers=True):
         """
         
@@ -28,10 +29,15 @@ class Machine:
         self._max_output_rate = max_output_rate
         self._output_rate = output_rate or (max_output_rate + min_output_rate) / 2
         self.output_product = output_product
-        self.suppliers = []
-        self.add_suppliers(suppliers or [])
+        self._suppliers = []
         self.next_machines = []
         self.is_on = is_on
+        self._delays = []
+
+        if len(delays or []) == 0:
+            delays = [0.0] * len(suppliers or [])
+        for i in range(len(suppliers or [])):
+            self.add_supplier(suppliers[i], delays[i])
 
         if test_suppliers:
             # Check if a subproduct is not being provided
@@ -39,14 +45,15 @@ class Machine:
             _new_suppliers = []
             for sub_product in output_product.sub_products_list:
                 sub_product_has_provider = False
-                for prev_machine in self.suppliers:
+                for prev_machine in self._suppliers:
                     if prev_machine.output_product == sub_product:
                         sub_product_has_provider = True
                 if not sub_product_has_provider:
                     print(f"Machine <{self.name}> has no provider for required input <{sub_product.name}>")
                     new_supplier = ExternalSupplier(output_product=sub_product, next_machines=[self])
                     _new_suppliers.append(new_supplier)
-            self.suppliers += _new_suppliers
+            for new_supplier in _new_suppliers:
+                self.add_supplier(new_supplier, 0.0)
 
     @property
     def min_performance(self):
@@ -72,19 +79,21 @@ class Machine:
     def max_performance(self, value):
         self._max_output_rate = value
 
+    @property
+    def suppliers(self):
+        return self._suppliers
+
     def add_next_machine(self, machine: 'Machine'):
         self.next_machines.append(machine)
 
-    def add_suppliers(self, suppliers):
-        if not isinstance(suppliers, list):
-            suppliers = [suppliers]
-        for supplier in suppliers:
-            self.suppliers.append(supplier)
-            supplier.add_next_machine(self)
+    def add_supplier(self, supplier, delay):
+        self._suppliers.append(supplier)
+        self._delays.append(delay)
+        supplier.add_next_machine(self)
 
     def to_dict(self):
         the_dict = {}
-        for supplier in self.suppliers:
+        for supplier in self._suppliers:
             if isinstance(supplier, Machine):
                 the_dict = {**the_dict, **supplier.to_dict()}
         the_dict[self.name] = {"min_output_rate": self._min_output_rate,
@@ -96,7 +105,8 @@ class Machine:
         return the_dict
 
     def get_graph(self):
-        graph = [[self.name, *[supplier.name for supplier in self.suppliers]]]
-        for supplier in self.suppliers:
+        graph = [[self.name, self._suppliers[i].name, self._delays[i]] for i in range(len(self._suppliers))]
+        for supplier in self._suppliers:
             graph += supplier.get_graph()
         return graph
+
