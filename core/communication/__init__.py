6c48fe4d127d3083061b9bb6b4a9f09e75652604
Nikita Sirons <kit.shiron@gmail.com>
2018-03-17 23:15:43 +0100

    new update

--- core/communication/__init__.py
+++ core/communication/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, session, g, redirect, url_for, safe_join, Response, \
-    abort, render_template, make_response, send_from_directory, send_file, has_request_context
+    abort, render_template, make_response, send_from_directory, send_file, has_request_context, jsonify
 from jinja2 import Markup, escape
 
 app = Flask(__name__)
@@ -13,9 +13,9 @@ def home():
 @app.route('/run_python', methods=["GET", "POST"])
 def run_python():
     json = request.get_json()
-    if json is not None:
-        print('DO PYTHON')
-    return render_template('export_data.html')
+    print('DO PYTHON')
+    new_json = json
+    return jsonify(new_json)
 
 
 if __name__ == '__main__':
